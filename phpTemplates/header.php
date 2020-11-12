d83d825f30e1aff61b12c3f5dfaa8dcad96456e1
Li Jie <lijie.leow@sjsu.edu>
2020-11-12 21:41:55 +0800

    change products page path for navbar in header.php

--- phpTemplates/header.php
+++ phpTemplates/header.php
@@ -10,7 +10,7 @@
                     <nav>
                         <ul>
                             <li><a href = "mainPage.php">Home</a></li>
-                            <li><a href = "products.php">Products</a></li>
+                            <li><a href = "category-allProducts.php">Products</a></li>
                             <li><a href = "">Contact</a></li>
                             <li><a href = "account.php">Account</a></li>
                             <li><a href = ""><i class="fa fa-shopping-cart" aria-hidden="true"></i></a></li>
