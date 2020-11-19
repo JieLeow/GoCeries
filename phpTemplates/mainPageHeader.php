a2eacb3a64b1db980925d3c3fbd2ee8a91e6295b
Li Jie <lijie.leow@sjsu.edu>
2020-11-20 06:34:29 +0800

    update main page header href

--- phpTemplates/mainPageHeader.php
+++ phpTemplates/mainPageHeader.php
@@ -29,7 +29,7 @@
                             <li><a href = "mainPage.php">Home</a></li>
                             <li><a href = "category-allProducts.php">Products</a></li>
                             <li><a href = "mainPage.php">Contact</a></li>
-                            <li><a href = "">Account</a></li>
+                            <li><a href = "account.php">Account</a></li>
                             <li><a href = ""><i class="fa fa-shopping-cart" aria-hidden="true"></i></a></li>
                         </ul>
                     </nav>
